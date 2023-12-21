7389fd5107fb244d7120f289fc6969cbfff4c6db
mmladjani <mmiljus24@gmail.com>
2023-12-21 22:01:58 +0100

    adding Player component

--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -3,16 +3,27 @@ import { useState } from 'react';
 function Player ({name, symbol}) {
 
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
 
-  const clickHandler = (value) => {
-    console.log(value)
-    setIsEditing(true);
+  const clickHandler = (buttonText) => {
+
+    if(buttonText === 'Edit'){
+      setIsEditing(true);
+    }else if(buttonText === 'Save'){
+      setIsEditing(false);
+    }
   }
+
+  const onChangeName = (inputValue) => {
+    setPlayerName(inputValue)
+  }
+
+  console.log(playerName);
 
   return (
       <li>
           <span className="player">
-            {isEditing ? <input className='player-name' placeholder='Please input your name' /> : <span className="player-name">{name}</span>}
+            {isEditing ? <input type="text" className='player-name' placeholder={playerName} onChange={(event) => onChangeName(event.target.value)}/> : <span className="player-name">{playerName}</span>}
             <span className="player-symbol">{symbol}</span>
             <button onClick={(event) => clickHandler(event.target.innerText)}>{isEditing ? 'Save' : 'Edit'}</button>
           </span>
